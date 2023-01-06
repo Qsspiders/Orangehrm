747175d823e78707317782f71741483c1b20da74
Qsspiders <122011454+Qsspiders@users.noreply.github.com>
2023-01-06 13:06:28 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,17 @@
+class demo1 
+{
+public static void main(String[]args)
+{
+int i=97;
+char ch='c';
+float f=ch;
+char ch=(char)i;
+float f=23.4f;
+int t=(int)f;
+System.out.println(t);
+System.out.println(ch);
+
+
+
+}
+}
